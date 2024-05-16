f144d55252e137b7030d0dc83e87b4e69ca21503
Aleli-code <maria.ale.rodriguez2023@gmail.com>
2024-05-15 22:01:22 -0300

    Puesta en funsión de la tabla imagenes con sus métodos

---
+++ src/imagenes/dto/create-imagen.dto.ts
@@ -0,0 +1,5 @@
+export class CreateImagenDto {
+    readonly name: string;
+    readonly description: string;
+  }
+  
---
+++ src/imagenes/dto/update-imagen.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateImagenDto {
+    readonly name: string;
+    readonly description: string;
+    
+  }
